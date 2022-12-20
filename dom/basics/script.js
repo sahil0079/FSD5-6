65f89fb806085d7eaf4f105a3873de1774b53afb
sahil0079 <114945558+sahil0079@users.noreply.github.com>
2022-12-20 21:35:28 +0530

    Add files via upload

---
+++ dom/basics/script.js
@@ -0,0 +1,82 @@
+// console.log(document.domain)
+// console.log(document.URL)
+
+// console.log(document.title)
+// document.title = 'abcde'
+
+// console.log(document.body)
+// console.log(document.head)
+
+// console.log(document.all[4])
+
+//add text
+
+// const body = document.body
+
+// body.append('hello', 'bye everyone')
+// body.appendChild('Hello')
+
+// const div = document.createElement('div')
+// console.log(div)
+
+//add text into the element
+// div.innerText = 'Javascript'
+
+// div.textContent = 'javascript'
+
+// body.append(div)
+// body.appendChild(div)
+
+// const div = document.querySelector('div')
+// console.log(div.innerText)
+// console.log(div.textContent)
+
+// const body = document.body
+
+// const div = document.createElement('div')
+
+// const strong = document.createElement('strong')
+
+// strong.innerText = 'My fav lang is js'
+
+// // div.innerText = '<strong>My fav lang is js </strong>'
+
+// div.append(strong)
+// div.innerHTML = '<strong>My fav lang is js </strong>'
+
+// body.append(div)
+
+// const div = document.querySelector('div')
+// const spanOne = document.getElementById('one')
+// const spanTwo = document.getElementById('two')
+
+//remove the elements
+
+// spanTwo.remove()
+// div.removeChild(spanOne)
+// div.append(spanTwo)
+
+//attributes methods
+// console.log(spanOne.getAttribute('name'))
+
+// spanOne.setAttribute('name', 'abcde')
+// spanOne.id = 'ndjksnkj'
+// spanOne.removeAttribute('name')
+
+//classes
+
+// spanTwo.classList.add('new-class')
+// spanTwo.classList.remove('new-class')
+
+// spanOne.classList.toggle('new-class')
+
+// spanOne.style.color = 'tomato'
+// spanTwo.style.backgroundColor = 'green'
+
+
+
+
+
+
+
+
