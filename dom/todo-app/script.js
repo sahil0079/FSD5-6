55067de7e66f62e9f66c15ffb85048208faf2cfe
sahil0079 <114945558+sahil0079@users.noreply.github.com>
2022-12-20 22:50:05 +0530

    Add files via upload

---
+++ dom/todo-app/script.js
@@ -0,0 +1,45 @@
+
+// console.log(document.getElementById('header'))
+
+// const headerTitle = document.getElementById('header-title')
+
+//get elements by class name
+
+// const listItems = document.getElementsByClassName('list-group-item')
+
+// for (let i = 0; i < listItems.length; i++) {
+//     listItems[i].style.backgroundColor = 'tomato'
+// }
+
+//get elements by tag name
+
+// const listItems = document.getElementsByTagName('li')
+
+// listItems[0].style.backgroundColor = 'tomato'
+
+// const buttons = document.getElementsByTagName('button')
+// console.log(buttons)
+
+//query selector
+
+// const header = document.querySelector('#header')
+// console.log(header)
+
+// const searchInput = document.querySelector('.form-control')
+// console.log(searchInput)
+
+// const listItems = document.querySelector('li')
+
+// listItems.style.backgroundColor = 'tomato'
+// console.log(listItems)
+
+
+//querySelectorAll
+
+// const listItems = document.querySelectorAll('li')
+// console.log(listItems)
+
+
+
+
+
